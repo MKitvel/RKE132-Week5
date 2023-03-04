7149250b9c985f21c565c55642126394d90e6639
Mattias Kitvel <mkitvel@tktk.ee>
2023-03-04 20:35:19 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,11 @@
+﻿//AND - &&
+//True && true ---> true
+//True && false ---> false
+//False && true ---> false
+//False && false ---> false
+
+//OR - II (pipes)
+//true II true ---> true
+//true II false ---> true
+//false II true ---> false
+//false II false ---> false
